0987e36e611a7bea3f91ea9e55e3b255036b0fe7
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-23 21:46:49 +0600

    added fake json data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import Doctors from "../Doctors/Doctors";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Doctors></Doctors>
     </div>
   );
 };
