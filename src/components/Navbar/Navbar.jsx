7fa6dc21be52725b71086743da43df92a3854f1a
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-23 08:59:23 +0600

    completed creating the navigation bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,91 @@
 import React from "react";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  // responsive submenu options
+  const linksResponsive = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>My-Bookings</a>
+      </li>
+      <li>
+        <a>Blogs</a>
+      </li>
+      <li>
+        <a>Contact us</a>
+      </li>
+    </>
+  );
+
+  //   main menu options
+  const linksMain = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+
+      <li>
+        <a>My-Bookings</a>
+      </li>
+      <li>
+        <a>Blogs</a>
+      </li>
+      <li>
+        <a>Contact Us</a>
+      </li>
+    </>
+  );
+
   return (
-    <div>
-      <h1>This is Navigation Bar</h1>
+    <div className="bg-[#EFEFEF] shadow-sm">
+      <div className="navbar px-[160px] py-[20px]">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {linksResponsive}
+            </ul>
+          </div>
+          <div className="flex gap-3">
+            <img className="h-[45px] w-[45px]" src={logo} alt="" />
+            <span className="text-xl font-[Plus_Jakarta_Sans] text-[32px] font-extrabold text-[rgb(15,15,15)]">
+              Phudu
+            </span>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-[rgba(15,15,15,0.7)] font-medium">
+            {linksMain}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <button className="btn text-center text-white text-xl bg-[#176AE5] rounded-[99px] px-7 py-4 mx-[10px] my-0">
+            Emergency
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
